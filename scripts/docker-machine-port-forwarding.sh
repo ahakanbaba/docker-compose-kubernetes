3064a20455d25429452f3b748bb1b5a7cff36f71
box-jhuffaker <jhuffaker@box.com>
2015-12-03 10:25:27 -0800

    Support docker-machine ssh for 0.5

--- scripts/docker-machine-port-forwarding.sh
+++ scripts/docker-machine-port-forwarding.sh
@@ -5,7 +5,12 @@
 function forward_port_if_not_forwarded {
     port=$1
     machine=$(docker-machine active)
-    forward_port_command="ssh -f -N -i $HOME/.docker/machine/machines/$machine/id_rsa -L $port:localhost:$port docker@$(docker-machine ip $machine)"
+    if docker-machine --version | grep 'docker-machine version 0.5' > /dev/null; then
+        forward_port_command="docker-machine ssh -f -N -L $port:localhost:$port $machine"
+    else
+        forward_port_command="ssh -f -N -i $HOME/.docker/machine/machines/$machine/id_rsa -L $port:localhost:$port docker@$(docker-machine ip $machine)"
+    fi
+
     existing_forward=$(ps ax | grep "$forward_port_command" | grep -v grep)
     
     if [ -z "$existing_forward" ]; then
@@ -15,4 +20,3 @@ function forward_port_if_not_forwarded {
 
 # Kubernetes API
 forward_port_if_not_forwarded 8080
-
